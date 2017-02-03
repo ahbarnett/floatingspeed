e3f7255df2791f89d606b3df0332f4dcfe3df95f
ahbarnett <ahb@math.dartmouth.edu>
2017-02-03 12:35:19 -0500

    -Ofast recovers fortran speed for all C/C++ complex and stl vector types

--- do_complextimings.sh
+++ do_complextimings.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 # make and run some basic complex arithmetic and RAM access speed tests.
 # Barnett 1/18/17, modified for Jeremy's tweaks.
+# 2/3/17: Pataki suggested -Ofast.
 
 # clean
 rm -f *.o complexmulttimingf complexmulttiming
 
-export FLAGS="-O3 -funroll-loops -march=native"
+export FLAGS="-Ofast"
+#export FLAGS="-Ofast -funroll-loops -march=native"
+#export FLAGS="-O3"
+
 # compile and run
 gfortran complexmulttiming.f -o complexmulttimingf $FLAGS
 echo Fortran:
 ./complexmulttimingf
 g++ utils.cpp -c
-g++ complexmulttiming.cpp utils.o -o complexmulttiming $FLAGS -D USE_C_TYPE_COMPLEX
+g++ complexmulttiming.cpp utils.o -o complexmulttimingc $FLAGS -D USE_C_TYPE_COMPLEX
 echo C and C-type:
-./complexmulttiming
-g++ complexmulttiming.cpp utils.o -o complexmulttiming $FLAGS -D USE_CPP_TYPE_COMPLEX
+./complexmulttimingc
+g++ complexmulttiming.cpp utils.o -o complexmulttimingcpp $FLAGS -D USE_CPP_TYPE_COMPLEX
 echo C++ type:
-./complexmulttiming
+./complexmulttimingcpp
