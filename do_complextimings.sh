e89dd648d13f4601680853f3bd5f44848f806e2d
ahbarnett <ahb@math.dartmouth.edu>
2017-03-28 15:48:44 -0400

    xeon vs i7 better testing of flags

--- do_complextimings.sh
+++ do_complextimings.sh
@@ -2,15 +2,15 @@
 # make and run some basic complex arithmetic and RAM access speed tests.
 # Barnett 1/18/17, modified for Jeremy's tweaks.
 # 2/3/17: Pataki suggested -Ofast.
-# 3/28/17: realised that unrolling or march-native needed to get full speed
-#          for C++ complex vector std types.
+# 3/28/17: realised that unrolling or march-native needed to get full speed for C++ complex vector std types on i7.
 
 # clean
 rm -f *.o complexmulttimingf complexmulttimingc complexmulttimingcpp
 
 #export FLAGS="-O3"  # not enough, has bad (2x slower) hit in C++ complex types
-export FLAGS="-Ofast"
+#export FLAGS="-Ofast"
 # either of following helps, esp for the final std C++ complex vector type...
+export FLAGS="-Ofast -funroll-loops"
 #export FLAGS="-Ofast -funroll-loops -march=native"
 
 # compile and run
@@ -21,9 +21,9 @@ echo Fortran:
 
 g++ utils.cpp -c
 g++ complexmulttiming.cpp utils.o -o complexmulttimingc $FLAGS -D USE_C_TYPE_COMPLEX
-echo C and C-type:
+echo C++-type real and C-type complex:
 ./complexmulttimingc
 
 g++ complexmulttiming.cpp utils.o -o complexmulttimingcpp $FLAGS -D USE_CPP_TYPE_COMPLEX
-echo C++ type:
+echo C++-type complex:
 ./complexmulttimingcpp
